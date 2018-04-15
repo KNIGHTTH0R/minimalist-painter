1858f8b492ea31970fe67adb649a8217a72a5811
benjbrandall <ben@process.st>
2018-04-15 11:04:03 +0300

    Add files via upload

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'https://rubygems.org'
+gem 'oily_png'
+gem 'twitter'
+gem 'formdata'
+gem 'rest-client'
+gem 'mime-types', '~> 3.1'
+gem 'whenever', require: false
---
+++ bot.rb
@@ -0,0 +1,46 @@
+require 'rubygems'
+require 'bundler/setup'
+require 'oily_png'
+require 'twitter'
+
+client = Twitter::REST::Client.new do |config| # authenticate with twitter API
+  config.consumer_key        = ""
+  config.consumer_secret     = ""
+  config.access_token        = ""
+  config.access_token_secret = ""
+end
+
+def generate_hex
+  alpha = ['a','b','c','d','e','f']
+  numeric = ['1','2','3','4','5','6']
+  valid_hex_values = alpha + numeric
+  generated_hex = Array.new
+  6.times do
+    generated_hex.push(valid_hex_values.sample)
+    end
+    generated_hex = generated_hex.join(",").gsub(",","")
+end
+
+def generate_image(hex)
+  png = ChunkyPNG::Image.new(256, 256, ChunkyPNG::Color::TRANSPARENT)
+  pixels = 256
+  pixels.times do |n|
+    pixels.times do |x|
+      png[n,x] = ChunkyPNG::Color.from_hex(hex)
+     end
+   end
+   png.save("images/#{hex}.png", :interlace => true)
+end
+
+def tweet(client)
+  generate_image(generate_hex)
+  painting = Dir.glob(File.join('images', '*.*')).max { |a,b| File.ctime(a) <=> File.ctime(b) } # the painting file path is the most recent file in the folder
+  painting_title = painting.slice(7,16).chomp('.png').upcase.prepend('#')
+  tweet = "UNTITLED, #{painting_title} ON 256X256 PIXEL CANVAS, #{Time.now.year}"
+  client.update_with_media(tweet,painting)
+end
+
+while true
+  tweet(client)
+  sleep(3600)
+end
